85b74246a1e7e9e23138a2f8b921f1917fcebebf
Andrea Bisognin <andrea.bisognin@gmail.com>
2014-08-07 20:02:26 +0200

    improved data loading

--- plot1.R
+++ plot1.R
@@ -1,10 +1,15 @@
+
 prepare.data <- function() {
-  power.data <- read.table("household_power_consumption.txt",sep=";",header=T,na.strings="?")
-  power.data$Date <- strptime(power.data$Date,"%d/%m/%Y")
-  start <- strptime("2007-02-01","%Y-%m-%d")
-  end <- strptime("2007-02-02","%Y-%m-%d")
-  subset(power.data, Date >= start & Date <= end)
+  power.data <- read.table("household_power_consumption.txt",sep=";",
+                           col.names=c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+"Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", 
+"Sub_metering_3"),header=F,na.strings="?",skip=66637,nrows=2880)
+  date.time <- paste(power.data$Date,power.data$Time)
+  power.data$date.time <- strptime(date.time,"%d/%m/%Y %H:%M:%S")
+  power.data
 }
+
+
 
 power.data <- prepare.data()
 png("plot1.png",width = 480,height = 480)
